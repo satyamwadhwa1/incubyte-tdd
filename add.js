97c47bf6fdafa6e079c273a56bfe8f752cd30a1b
satyamwadhwa1 <93925848+satyamwadhwa1@users.noreply.github.com>
2025-03-08 10:32:02 +0000

    Refactor: Clean up edge cases

--- add.js
+++ add.js
@@ -1,23 +1,22 @@
 function add(numbers) {
     if (!numbers) return 0;
-    
+
     let delimiter = /[,\n]/;
     if (numbers.startsWith('//')) {
-      let customDelimiter = numbers.substring(2, 3);
-      numbers = numbers.substring(4); // Ignore the "//;" part and newline
-  
-      delimiter = new RegExp(customDelimiter);
+        let customDelimiter = numbers.substring(2, 3);
+        numbers = numbers.substring(4); // Ignore the "//;" part and newline
+
+        delimiter = new RegExp(customDelimiter);
     }
-  
+
     const nums = numbers.split(delimiter).map(Number);
     const negatives = nums.filter(n => n < 0);
-    
+
     if (negatives.length > 0) {
-      throw new Error(`negative numbers not allowed: ${negatives.join(',')}`);
+        throw new Error(`negative numbers not allowed: ${negatives.join(',')}`);
     }
-  
+
     return nums.reduce((acc, num) => acc + num, 0);
-  }
-  
-  module.exports = add;
-  
+}
+
+module.exports = add;
--- add.test.js
+++ add.test.js
@@ -1,37 +1,37 @@
 const add = require('./add');
 
 test('empty string returns 0', () => {
-  expect(add("")).toBe(0);
+    expect(add("")).toBe(0);
 });
-
 
 test('single number returns itself', () => {
     expect(add("5")).toBe(5);
-  });
-  
+});
 
 test('two comma-separated numbers return sum', () => {
     expect(add("1,5")).toBe(6);
-  });
+});
 
-  test('any amount of numbers', () => {
+test('any amount of numbers', () => {
     expect(add("1,2,3,4")).toBe(10);
-  });
+});
 
 test('handle newline delimiter', () => {
     expect(add("1\n2,3")).toBe(6);
-  });
+});
 
-  test('custom delimiter', () => {
+test('custom delimiter', () => {
     expect(add("//;\n1;2")).toBe(3);
-  });
-  
-  test('throw error for negatives', () => {
-    expect(() => add("1,-2")).toThrow("negative numbers not allowed: -2");
-  });
+});
 
-  test('multiple negatives in error message', () => {
+test('throw error for negatives', () => {
+    expect(() => add("1,-2")).toThrow("negative numbers not allowed: -2");
+});
+
+test('multiple negatives in error message', () => {
     expect(() => add("1,-2,3,-5")).toThrow("negative numbers not allowed: -2,-5");
-  });
-  
-  
+});
+
+test('empty part between delimiters', () => {
+    expect(add("1,,2")).toBe(3);
+});
